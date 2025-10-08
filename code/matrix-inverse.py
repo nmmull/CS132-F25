710e54757ceb226309cd6119552df339ed131f38
Nathan Mull <nmmull@gmail.com>
2025-10-08 14:46:37 -0400

    update webpage

--- code/matrix-inverse.py
+++ code/matrix-inverse.py
@@ -13,6 +13,11 @@ ainv = np.linalg.inv(a)
 aid = np.hstack([a, np.eye(3), np.array([[5, -1, 6]]).T])
 
 # row reductions
+aid[2] -= aid[0]
+aid[2] += 2 * aid[1]
+aid[1] -= 3 * aid[2]
+aid[0] -= 5 * aid[2]
+aid[0] -= 2 * aid[1]
 
 print()
 print("------------------------------")
@@ -20,23 +25,23 @@ print("The matrix [ A I ]:")
 print("------------------------------")
 print(aid[:,:6])
 
-# print()
-# print("------------------------------")
-# print("Augmented matrix for Ax = e1:")
-# print("------------------------------")
-# print(aid[:,:4])
+print()
+print("------------------------------")
+print("Augmented matrix for Ax = e1:")
+print("------------------------------")
+print(aid[:,:4])
 
-# print()
-# print("------------------------------")
-# print("Augmented matrix for Ax = e2:")
-# print("------------------------------")
-# print(aid[:,[0, 1, 2, 4]])
+print()
+print("------------------------------")
+print("Augmented matrix for Ax = e2:")
+print("------------------------------")
+print(aid[:,[0, 1, 2, 4]])
 
-# print()
-# print("------------------------------")
-# print("Augmented matrix for Ax = e3:")
-# print("------------------------------")
-# print(aid[:,[0, 1, 2, 5]])
+print()
+print("------------------------------")
+print("Augmented matrix for Ax = e3:")
+print("------------------------------")
+print(aid[:,[0, 1, 2, 5]])
 
 print()
 print("------------------------------")
@@ -46,11 +51,11 @@ print(aid[:,[0, 1, 2, 6]])
 
 
 
-# print()
-# print("------------------------------")
-# print("A^{-1}:")
-# print("------------------------------")
-# print(aid[:,3:6])
+print()
+print("------------------------------")
+print("A^{-1}:")
+print("------------------------------")
+print(aid[:,3:6])
 
 
 print()
